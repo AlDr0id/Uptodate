05640115d8c630182dc8f0184e00806b8ed38a14
Álvaro de la Cruz <al.dr0id.100@gmail.com>
2018-02-07 18:02:03 +0100

    Update uptodate.sh

--- uptodate.sh
+++ uptodate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 apt-get update -y
+# msfupdate # Uncomment if you don't have Metasploit's repositories in your /etc/apt/sources.list
+# nikto -update # Uncomment if you have Nikto installed on your computer
 apt-get dist-upgrade -y
 apt-get upgrade -y
 apt autoremove -y
